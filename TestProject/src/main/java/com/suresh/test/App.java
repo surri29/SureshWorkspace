1e0b12d43701684c656b812c733a4fbe3bfb42ba
Suresh Ramasamy <surri28@gmail.com>
2013-01-08 10:46:05 -0500

    test

---
+++ TestProject/src/main/java/com/suresh/test/App.java
@@ -0,0 +1,13 @@
+package com.suresh.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World My Test Program!" );
+    }
+}
