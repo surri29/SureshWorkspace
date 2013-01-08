610566beb003c90232fcd872d22a4cda92e2a1ca
Suresh Ramasamy <surri28@gmail.com>
2013-01-08 11:37:20 -0500

    Added merchant delegate

---
+++ TestProject/src/main/java/com/suresh/gw/MerchantDelegate.java
@@ -0,0 +1,14 @@
+package com.suresh.gw;
+
+public class MerchantDelegate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is merchant delegate");
+	}
+
+}
